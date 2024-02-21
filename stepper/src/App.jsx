811d4327e7f4fcc64755f423359c9e4f00887a13
SandhyaR1007 <grajwanshi1008@gmail.com>
2024-02-21 21:45:00 +0530

    update: steps logic

--- stepper/src/App.jsx
+++ stepper/src/App.jsx
@@ -1,5 +1,31 @@
+import CheckoutStepper from "./components/CheckoutStepper";
+import "./App.css";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <CheckoutStepper checkoutSteps={checkoutSteps} />
+    </div>
+  );
 };
 
 export default App;
+
+const checkoutSteps = [
+  {
+    name: "Customer Info",
+    Component: () => <div>Provide your contact details.</div>,
+  },
+  {
+    name: "Shipping Info",
+    Component: () => <div>Enter your shipping address.</div>,
+  },
+  {
+    name: "Payment",
+    Component: () => <div>Complete payment for your order.</div>,
+  },
+  {
+    name: "Delivered",
+    Component: () => <div> Your order has been delivered.</div>,
+  },
+];
