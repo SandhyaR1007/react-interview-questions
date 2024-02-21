9679e4c833c73e23c844b61ac31e1d2ca21e1520
SandhyaR1007 <grajwanshi1008@gmail.com>
2024-02-21 22:07:51 +0530

    update: conditionals for step markers

--- stepper/src/components/CheckoutStepper.jsx
+++ stepper/src/components/CheckoutStepper.jsx
@@ -3,20 +3,31 @@ import { useState } from "react";
 const CheckoutStepper = ({ checkoutSteps }) => {
   const [currentStep, setCurrentStep] = useState(0);
   const handleStep = () => {
-    if (currentStep < checkoutSteps.length - 1)
-      setCurrentStep((step) => step + 1);
+    if (currentStep < checkoutSteps.length) setCurrentStep((step) => step + 1);
   };
+  const stepClass = (step) =>
+    step === currentStep
+      ? "current-step"
+      : step < currentStep
+      ? "completed-step"
+      : "";
   return (
     <div className="">
       <div className="checkoutStepsContainer">
         {checkoutSteps.map(({ name }, i) => (
-          <div key={i}>
-            <span className="circle"></span>
+          <div key={i} className="stepContainer">
+            <span className={`circle ${stepClass(i)}`}>
+              {i < currentStep ? "✔️" : i + 1}
+            </span>
             <span>{name}</span>
           </div>
         ))}
       </div>
-      <p>{checkoutSteps[currentStep].Component()}</p>
+      <p>
+        {currentStep === checkoutSteps.length
+          ? checkoutSteps[currentStep - 1].Component()
+          : checkoutSteps[currentStep].Component()}
+      </p>
       <button onClick={handleStep}>Next</button>
     </div>
   );
