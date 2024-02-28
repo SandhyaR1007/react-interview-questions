65326fb3fffa5e6b586dc53d037ebd767678b3a2
SandhyaR1007 <grajwanshi1008@gmail.com>
2024-02-28 18:00:48 +0530

    update: progress logic for steps

--- stepper/src/components/CheckoutStepper.jsx
+++ stepper/src/components/CheckoutStepper.jsx
@@ -1,25 +1,57 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const CheckoutStepper = ({ checkoutSteps }) => {
+const CheckoutStepper = ({ checkoutSteps = [] }) => {
   const [currentStep, setCurrentStep] = useState(0);
+  const [margins, setMargins] = useState({
+    marginLeft: 0,
+    marginRight: 0,
+  });
+
+  const stepRef = useRef([]);
+  useEffect(() => {
+    if (stepRef.current.length)
+      setMargins({
+        marginLeft: stepRef.current[0].offsetWidth / 2,
+        marginRight: stepRef.current[checkoutSteps.length - 1].offsetWidth / 2,
+      });
+  }, [checkoutSteps.length, stepRef]);
+
   const handleStep = () => {
     if (currentStep < checkoutSteps.length) setCurrentStep((step) => step + 1);
   };
   const stepClass = (step) =>
     step === currentStep ? "active" : step < currentStep ? "completed" : "";
+  const calculateProgress = () => (currentStep / checkoutSteps.length) * 100;
+  if (!checkoutSteps.length) return <></>;
   return (
     <div className="container">
       <div className="checkoutStepsContainer">
         {checkoutSteps.map(({ name }, i) => (
-          <div key={i} className="stepContainer">
+          <div
+            key={name}
+            className="stepContainer"
+            ref={(el) => {
+              stepRef.current.push(el);
+            }}
+          >
             <span className={`step ${stepClass(i)}`}>
               {i < currentStep ? <span>&#10003;</span> : i + 1}
             </span>
             <span>{name}</span>
           </div>
         ))}
-        <div className="progress-bar">
-          <div className="progress"></div>
+        <div
+          className="progress-bar"
+          style={{
+            width: `calc(100% - ${margins.marginLeft + margins.marginRight}px)`,
+            marginLeft: margins.marginLeft,
+            marginRight: margins.marginRight,
+          }}
+        >
+          <div
+            className="progress"
+            style={{ width: `${calculateProgress()}%` }}
+          ></div>
         </div>
       </div>
 
