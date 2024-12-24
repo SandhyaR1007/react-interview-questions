dfe8e42b09ff7ad5a85c20fe635c3e1813482019
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-12-24 18:42:12 +0530

    update: add pieces and bg color for squares

--- src/components/chessboard/ChessBoard.jsx
+++ src/components/chessboard/ChessBoard.jsx
@@ -4,13 +4,14 @@ import Square from "./Square";
 
 const ChessBoard = () => {
   let chessArr = new Array(8).fill(null);
-  console.log(chessArr);
   return (
     <div className="board">
       {chessArr.map((arr, idx) => (
         <div style={{ display: "flex" }} key={`${idx}_outer`}>
           {chessArr.map((ele, index) => (
             <Square
+              outerPos={idx}
+              innerPos={index}
               key={`${idx + index}_inner`}
               color={(idx + index) % 2 === 0 ? 0 : 1}
             />
--- src/components/chessboard/Square.jsx
+++ src/components/chessboard/Square.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 
-const Square = ({ color }) => {
-  console.log(color);
-  return <div className={`square ${color === 0 ? "black" : "white"}`}></div>;
+const Square = ({ color, outerPos, innerPos }) => {
+  return (
+    <div className={`square ${color === 0 ? "darkBg" : "lightBg"}`}>
+      {getElement(outerPos, innerPos)}
+    </div>
+  );
 };
 
 export default Square;
+const getElement = (outerPos, innerPos) => {
+  if (outerPos === 0) return whites[innerPos];
+  if (outerPos === 7) return blacks[innerPos];
+  if (outerPos === 1) return whitePawn;
+  if (outerPos === 6) return blackPawn;
+};
+
+const whites = {
+  0: "♖",
+  7: "♖",
+  1: "♘",
+  6: "♘",
+  2: "♗",
+  5: "♗",
+  4: "♔",
+  3: "♕",
+};
+const blackPawn = "♟";
+const whitePawn = "♙";
+const blacks = {
+  0: "♜",
+  7: "♜",
+  1: "♞",
+  6: "♞",
+  2: "♝",
+  5: "♝",
+  4: "♛",
+  3: "♚",
+};
