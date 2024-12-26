31825424358a619f1958f073b9753d47a038f5ca
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-12-26 12:42:53 +0530

    update: made pieces droppable

--- src/components/positionofbishop/BishopGrid.tsx
+++ src/components/positionofbishop/BishopGrid.tsx
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
 import GridSquare from "./GridSquare";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const BishopGrid = () => {
   const grid = new Array(8).fill(null);
   const [piecePosition, setPiecePosition] = useState([0, 0]);
   return (
-    <div className="board">
-      {grid.map((g, outerIndex) => (
-        <div style={{ display: "flex" }} key={outerIndex}>
-          {grid.map((_, innerIndex) => (
-            <GridSquare
-              key={innerIndex + outerIndex}
-              outerIndex={outerIndex}
-              innerIndex={innerIndex}
-              piecePosition={piecePosition}
-              setPiecePosition={setPiecePosition}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="board">
+        {grid.map((g, outerIndex) => (
+          <div style={{ display: "flex" }} key={outerIndex}>
+            {grid.map((_, innerIndex) => (
+              <GridSquare
+                key={innerIndex + outerIndex}
+                outerIndex={outerIndex}
+                innerIndex={innerIndex}
+                piecePosition={piecePosition}
+                setPiecePosition={setPiecePosition}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    </DndProvider>
   );
 };
 
