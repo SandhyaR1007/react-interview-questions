53c7233040cbf305685fb387d3e90e75a11f68c6
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-12-26 11:29:42 +0530

    update: initial bishop positions logic

--- src/components/positionofbishop/BishopGrid.tsx
+++ src/components/positionofbishop/BishopGrid.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BishopGrid = () => {
   const grid = new Array(8).fill(null);
+  const [bishopPositions, setBishopPositions] = useState([]);
   return (
     <div className="board">
       {grid.map((g, outerIndex) => (
         <div style={{ display: "flex" }}>
           {grid.map((_, innerIndex) => (
-            <div
-              className={`square`}
-              style={{
-                backgroundColor:
-                  (innerIndex + outerIndex) % 2 == 0 ? "black" : "white",
-              }}
-            ></div>
+            <GridSquare
+              outerIndex={outerIndex}
+              innerIndex={innerIndex}
+              bishopPositions={bishopPositions}
+              setBishopPositions={setBishopPositions}
+            />
           ))}
         </div>
       ))}
@@ -22,3 +22,50 @@ const BishopGrid = () => {
 };
 
 export default BishopGrid;
+
+const GridSquare = ({
+  outerIndex,
+  innerIndex,
+  bishopPositions,
+  setBishopPositions,
+}) => {
+  function clickHandler(innerIndex, outerIndex) {
+    let result: any[] = [];
+    //top left
+    for (let i = outerIndex - 1, j = innerIndex - 1; i >= 0; i--) {
+      result.push([i, j]);
+      j--;
+    }
+    //top right
+    for (let i = outerIndex - 1, j = innerIndex + 1; i >= 0; i--) {
+      result.push([i, j]);
+      j++;
+    }
+    //bottom left
+    for (let i = outerIndex + 1, j = innerIndex - 1; i < 8; i++) {
+      result.push([i, j]);
+      j--;
+    }
+    //bottom right
+    for (let i = outerIndex + 1, j = innerIndex + 1; i < 8; i++) {
+      result.push([i, j]);
+      j++;
+    }
+
+    setBishopPositions(result);
+  }
+  const isPos = bishopPositions.find(
+    ([a, b]) => a == outerIndex && b === innerIndex
+  );
+  return (
+    <div
+      onMouseOver={() => clickHandler(innerIndex, outerIndex)}
+      className={`square ${
+        (innerIndex + outerIndex) % 2 == 0 ? "darkBg" : "lightBg"
+      } ${isPos ? "hoveredBg" : ""}`}
+      style={{
+        fontSize: "1.5rem",
+      }}
+    ></div>
+  );
+};
